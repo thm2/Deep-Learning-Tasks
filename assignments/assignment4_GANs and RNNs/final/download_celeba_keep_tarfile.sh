c4cefa9b3f153a70dd7f329b1bd5e4bb1af762d6
thm2 <98324965+thm2@users.noreply.github.com>
2023-02-07 16:02:59 -0600

    Add files via upload

---
+++ assignments/assignment4_GANs and RNNs/final/download_celeba_keep_tarfile.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# download 
+wget https://uofi.box.com/shared/static/q4pf89jtkvjndi4f8ip7wofuulhhphjj.zip
+mkdir celeba_data
+unzip q4pf89jtkvjndi4f8ip7wofuulhhphjj.zip -d celeba_data
